0bf213fb077147aa77fbe5df132074c3ec8e5e30
brian <kipkoechbrian0@gmail.com>
2022-10-24 16:59:54 +0300

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :length, :width, :height
+  def initialize(length, width, height)
+    @length = length
+    @height = height
+    @width = width
+  end
+
+  def kind
+    if length <= 0 || width <= 0 || height <= 0 || length + width <=height || length + height<= width || width+ height <= length
+      raise TriangleError
+    end
+    if length == width &&width == height
+      return :equilateral
+    elsif length == width || length == height || width==height
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
